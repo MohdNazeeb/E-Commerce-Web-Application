1b205961db0801b8a6ab75ff5c41a8e3073df93b
Mohd Nazeeb Mansoori <warsinazeeb@gmail.com>
2026-04-25 03:05:13 +0530

    feat: add DTO pattern for getAllCategories API response (#1)

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,12 +1,13 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryResponse;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface CategoryService {
-    public List<Category> getAllCategories();
+    public CategoryResponse getAllCategories();
     public void createCategory(Category category);
     public String deleteCategory(Long categoryId);
     public Category updateCategory(Category category, Long categoryId);
